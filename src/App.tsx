c8962a4f12ca7d11a44327753842858c81f3886a
tolgaozkan <tolgaozkan@gmail.com>
2020-06-16 23:29:13 +0200

    Apply lint and formatter

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,7 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-import React, { useEffect, useState } from "react";
-import { API, graphqlOperation } from "aws-amplify";
-import { createTodo } from "./graphql/mutations";
-import { listTodos } from "./graphql/queries";
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { createTodo } from './graphql/mutations';
+import { listTodos } from './graphql/queries';
 
 interface ToDo {
   id: string;
@@ -36,11 +9,52 @@ interface ToDo {
   description: string;
 }
 
-const initialState = { name: "", description: "" };
+const styles = {
+  container: {
+    width: 400,
+    margin: '0 auto',
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column' as 'column',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  todo: { marginBottom: 15 },
+  input: {
+    border: 'none',
+    backgroundColor: '#ddd',
+    marginBottom: 10,
+    padding: 8,
+    fontSize: 18,
+  },
+  todoName: { fontSize: 20, fontWeight: 'bold' as 'bold' },
+  todoDescription: { marginBottom: 0 },
+  button: {
+    backgroundColor: 'black',
+    color: 'white',
+    outline: 'none',
+    fontSize: 18,
+    padding: '12px 0px',
+  },
+};
+
+const initialState = { name: '', description: '' };
 
 const App = () => {
   const [formState, setFormState] = useState(initialState);
   const [todos, setTodos] = useState<ToDo[]>([]);
+
+  async function fetchTodos() {
+    try {
+      const todoData = (await API.graphql(graphqlOperation(listTodos))) as {
+        data: { listTodos: { items: ToDo[] } };
+      };
+      const todoItems = todoData.data.listTodos.items;
+      setTodos(todoItems);
+    } catch (err) {
+      console.log('error fetching todos');
+    }
+  }
 
   useEffect(() => {
     fetchTodos();
@@ -48,18 +62,6 @@ const App = () => {
 
   function setInput(key: string, value: string) {
     setFormState({ ...formState, [key]: value });
-  }
-
-  async function fetchTodos() {
-    try {
-      const todoData = (await API.graphql(graphqlOperation(listTodos))) as {
-        data: { listTodos: { items: ToDo[] } };
-      };
-      const todos = todoData.data.listTodos.items;
-      setTodos(todos);
-    } catch (err) {
-      console.log("error fetching todos");
-    }
   }
 
   async function addTodo() {
@@ -70,7 +72,7 @@ const App = () => {
       setFormState(initialState);
       await API.graphql(graphqlOperation(createTodo, { input: todo }));
     } catch (err) {
-      console.log("error creating todo:", err);
+      console.log('error creating todo:', err);
     }
   }
 
@@ -78,18 +80,18 @@ const App = () => {
     <div style={styles.container}>
       <h2>Amplify Todos</h2>
       <input
-        onChange={(event) => setInput("name", event.target.value)}
+        onChange={(event) => setInput('name', event.target.value)}
         style={styles.input}
         value={formState.name}
         placeholder="Name"
       />
       <input
-        onChange={(event) => setInput("description", event.target.value)}
+        onChange={(event) => setInput('description', event.target.value)}
         style={styles.input}
         value={formState.description}
         placeholder="Description"
       />
-      <button style={styles.button} onClick={addTodo}>
+      <button type="button" style={styles.button} onClick={addTodo}>
         Create Todo
       </button>
       {todos.map((todo, index) => (
@@ -100,35 +102,6 @@ const App = () => {
       ))}
     </div>
   );
-};
-
-const styles = {
-  container: {
-    width: 400,
-    margin: "0 auto",
-    display: "flex",
-    flex: 1,
-    flexDirection: 'column' as 'column',
-    justifyContent: "center",
-    padding: 20,
-  },
-  todo: { marginBottom: 15 },
-  input: {
-    border: "none",
-    backgroundColor: "#ddd",
-    marginBottom: 10,
-    padding: 8,
-    fontSize: 18,
-  },
-  todoName: { fontSize: 20, fontWeight: 'bold' as 'bold' },
-  todoDescription: { marginBottom: 0 },
-  button: {
-    backgroundColor: "black",
-    color: "white",
-    outline: "none",
-    fontSize: 18,
-    padding: "12px 0px",
-  },
 };
 
 export default App;
