d1035f75cc3676a71a1f5445126a049a84381886
Serkan <serkany32@hotmail.com>
2020-11-18 21:33:18 +0300

    validate-pin initial upload

--- src/components/Init.tsx
+++ src/components/Init.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Subscription } from 'rxjs';
-import { Row, Col, Button, Spinner } from 'react-bootstrap';
+import { Row, Col, Button, Spinner, Modal } from 'react-bootstrap';
 import generateCode from '../utils/generate-code';
-import enterCode from '../utils/enter-code';
+import { enterCode, enterPin } from '../utils/enter-code';
 import listenConnection from '../utils/listen-connection';
 import InputBox from './InputBox';
 import QR from './QR';
 import QRReader from './QRReader';
 import CopyText from './CopyText';
-import { Connector, Connected } from '../types';
+import { Connector, Connected, Connection } from '../types';
 import listenConnectionDone from '../utils/listen-connection-done';
 
 type InitState = {
@@ -17,6 +17,9 @@ type InitState = {
   connector: Connector;
   readQR: boolean;
   buttonDisabled: boolean;
+  hasPin: boolean;
+  enteredPin: number;
+  connection: Connection;
 };
 type InitProps = {
   onConnected?: (data: Connected) => void;
@@ -33,6 +36,9 @@ class Init extends React.Component<InitProps, InitState> {
       generatedCode: null,
       readQR: false,
       buttonDisabled: true,
+      hasPin: false,
+      enteredPin: null,
+      connection: null,
     };
   }
 
@@ -74,8 +80,16 @@ class Init extends React.Component<InitProps, InitState> {
     }
     const updatedConnector = await enterCode(enteredCode, connector);
     if (updatedConnector) {
-      this.setState({ connector: updatedConnector });
-      this.onConnected();
+      if (updatedConnector.connector) {
+        this.setState({
+          hasPin: true,
+          connector: updatedConnector.connector,
+          connection: updatedConnector.connection,
+        });
+      } else {
+        this.setState({ connector: updatedConnector });
+        this.onConnected();
+      }
     }
   };
 
@@ -90,6 +104,34 @@ class Init extends React.Component<InitProps, InitState> {
           this.setState({
             enteredCode: code,
             buttonDisabled: val.length !== 6,
+          });
+        }
+      }
+    }
+  };
+
+  enterPin = async () => {
+    const { enteredPin, connector, connection } = this.state;
+    if (!enteredPin || enteredPin.toString().length !== 4) {
+      return;
+    }
+    const updatedConnector = await enterPin(connector, connection, enteredPin);
+    if (updatedConnector) {
+      this.setState({ hasPin: false, connector: updatedConnector });
+      this.onConnected(enteredPin);
+    }
+  };
+
+  pinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value;
+    if (!val) {
+      this.setState({ enteredCode: null });
+    } else {
+      const pin = parseInt(e.target.value, 10);
+      if (!Number.isNaN(pin)) {
+        if (val.length <= 4) {
+          this.setState({
+            enteredPin: pin,
           });
         }
       }
@@ -112,7 +154,14 @@ class Init extends React.Component<InitProps, InitState> {
   };
 
   render() {
-    const { generatedCode, readQR, buttonDisabled, enteredCode } = this.state;
+    const {
+      generatedCode,
+      readQR,
+      buttonDisabled,
+      enteredCode,
+      hasPin,
+      enteredPin,
+    } = this.state;
     return (
       <>
         {!readQR && (
@@ -174,6 +223,23 @@ class Init extends React.Component<InitProps, InitState> {
             />
           </Col>
         </Row>
+        <Modal
+          show={hasPin}
+          size="sm"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Body>
+            <InputBox
+              changeHandler={this.pinChange}
+              clickHandler={this.enterPin}
+              inputPlaceholder="Enter Pin"
+              buttonText="JOIN"
+              inputType="number"
+              inputValue={enteredPin && enteredPin.toString()}
+            />
+          </Modal.Body>
+        </Modal>
       </>
     );
   }
--- src/utils/enter-code.ts
+++ src/utils/enter-code.ts
@@ -1,41 +1,47 @@
 import { API, graphqlOperation } from 'aws-amplify';
 import { updateConnector } from '../graphql/mutations';
 import getConnectionByCode from './get-connection-by-code';
-import { Connector } from '../types';
+import { Connection, Connector } from '../types';
 import validatePin from './validate-pin';
 
-const enterCode = async (
-  code: number,
-  connector: Connector
-): Promise<Connector> => {
+const updateConnectorWrapper = async (input: {
+  id: string;
+  connectorConnectionId: string;
+}) => {
+  return (await API.graphql(graphqlOperation(updateConnector, { input }))) as {
+    data: { updateConnector: Connector };
+  };
+};
+
+const enterPin = async (
+  connector: Connector,
+  connection: Connection,
+  pin?: number
+) => {
+  if (pin) {
+    if (!(await validatePin(pin, connection.id))) {
+      // eslint-disable-next-line no-console
+      console.log('INVALID PIN');
+      return null;
+    }
+  }
+  return (
+    await updateConnectorWrapper({
+      id: connector.id,
+      connectorConnectionId: connection.id,
+    })
+  ).data.updateConnector;
+};
+
+const enterCode = async (code: number, connector: Connector): Promise<any> => {
   const connection = await getConnectionByCode(code);
   if (!connection) {
     return null;
   }
 
   if (connection.hasPin) {
-    // eslint-disable-next-line no-console
-    console.log('ENTER PIN: set window.pin');
-    // eslint-disable-next-line no-console
-    console.log('window.pin = 1234');
-    if (
-      !(window as any).pin ||
-      !(await validatePin((window as any).pin, connection.id))
-    ) {
-      // eslint-disable-next-line no-console
-      console.log('INVALID PIN');
-      return null;
-    }
+    return { connector, connection };
   }
-
-  const updateConnectorWrapper = async (input: {
-    id: string;
-    connectorConnectionId: string;
-  }) => {
-    return (await API.graphql(
-      graphqlOperation(updateConnector, { input })
-    )) as { data: { updateConnector: Connector } };
-  };
 
   return (
     await updateConnectorWrapper({
@@ -45,4 +51,4 @@ const enterCode = async (
   ).data.updateConnector;
 };
 
-export default enterCode;
+export { enterCode, enterPin };
